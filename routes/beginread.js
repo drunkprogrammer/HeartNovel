83fa1c1c27fc87eb6348dfb567f2ae524cd9940a
jam <915975732@qq.com>
2017-10-12 22:56:17 +0800

    增加开始阅读功能

--- routes/beginread.js
+++ routes/beginread.js
@@ -1,91 +1,29 @@
-var express = require('express');
-router = express.Router();
-var proxy = require('http-proxy-middleware');
-var app = express();
-var options = {
-		target: "http://api.zhuishushenqi.com/",
-		changeOrigin: true,
-		ws: true,
-		pathRewrite: {
-			'^/api': '/',
-			'^/chapter': '/chapter'
-		},
-		router: {
-			'localhost': 'http://localhost:8000'
-		},
-		changeOrigin: true
-	}
-var exampleProxy = proxy(options);
-app.use('/api', exampleProxy);
-var chapterdata = res.write(data);
-json = JSON.parse(chapterdata);
-console.log(json);
-//http://api.zhuishushenqi.com/toc/577b477dbd86a4bd3f8bf1b2?view=chapters
-//追书神器的api
+ var express=require("express");
+ var proxy=require("http-proxy-middleware");
+ var router=express.Router();
+ var app=express();
 
-//require('opn')('http://api.zhuishushenqi.com');
-
-//function onProxyReq(proxyReq,req,res){
-//	res.on('end',function(){
-//		var chapterdata = res.write(data);
-//		json=JSON.parse(chapterdata);
-//		console.log(json);
-//	}
-//}
-//function onError(err, req, res) {
-//	if(err) {
-//		console.log("this is an fail request");
-//	} else {
-//	
-//		var json = (eval('(' + chapterdata + ')')).chapters;
-//		console.log(json.title);
-//	}
-//}
-//var json=(eval('('+data+')')).chapters;
-//console.log(data);
-//function onProxyRes(proxyRes,req,res){
-//	console.log(res.toLocaleString());
-//}
-//router.get('/api', function(req, res) {
-//	res.on('end', function() {
-//		var options = {
-//			target: "http://api.zhuishushenqi.com/",
-//			changeOrigin: true,
-//			ws: true,
-//			pathRewrite: {
-//				'^/api': '/',
-//				'^/chapter': '/chapter'
-//			},
-//			router: {
-//				'localhost': 'http://localhost:8000'
-//			},
-//			changeOrigin: true
-//		}
-//		var exampleProxy = proxy(options);
-//		app.use('/api', exampleProxy);
-//		var chapterdata = res.write(data);
-//		json = JSON.parse(chapterdata);
-//		console.log(json);
-//	});
-//})
-router.get('/beginread', function(req, res) {
-	//res.location('/newnovel'); 
-	//	var json='';
-	//	res.on('data',function(message){
-	//		json + =message;
-	//	});
-	//	res.on('end',function(){
-	//		json=JSON.parse(json);
-	//		console.log(json);
-	//	})
-	//res.on('end',function(){
-	//		var chapterdata = res.write(data);
-	//		json=JSON.parse(chapterdata);
-	//		console.log(json);
-	//	}
-	res.sendFile(__dirname + "/" + "beginread");
-
-	//	}
-});
-
-module.exports = router;
+ /*
+章节内容api: chapter2.zhuishushenqi.com/chapter/章节link(从章节列表中获得)?k=2124b73d7e2e1945&t=1468223717
+example:    chapter2.zhuishushenqi.com/chapter/http%3a%2f%2fbook.my716.com%2fgetBooks.aspx%3fmethod%3dcontent%26bookId%3d1127281%26chapterFile%3dU_1212539_201701211420571844_4093_2.txt?k=2124b73d7e2e1945&t=1468223717
+method: GET
+ */
+ var options={
+     target:"http://chapter2.zhuishushenqi.com",
+     changeOrigin:true,
+     pathwrite:{
+        '^/chapter2':'/'
+     },
+     ws:true,
+     router:{
+     	'localhost':'http://localhost:8000'
+     }
+ }
+ var pro=proxy(options);
+ app.use('/chapter2',pro);
+ var content_data=res.write(data);
+ console.log(content_data);
+ res.get('/beginread',function (req,res) {
+ 	// body...
+ 	res.sendFile(__dirname+"/"+"beginread");
+ })
